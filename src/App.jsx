01eb11a909e9408a108c9850468c82818feb7bfa
Johnatan Gonzalez Velez <jgonzalez@sistecredito.com>
2021-06-18 17:14:26 -0500

    Creando plantilla para cualquier tipo de proyecto en react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// importacion
+import React from 'react'
+
+
+// generacion de la funcion componentes
+const App = () => {
+    // lo que ejecuta la funcion
+    return <h1>Calculadora (App) </h1>
+}
+//  exportacion hacia otros componenets
+export default App
+
+
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// importar el componente
+
+//utilizar el componente
+
+
+ReactDOM.render(
+ <h1>Calculadora</h1>,
+  document.getElementById('root')
+)
+
