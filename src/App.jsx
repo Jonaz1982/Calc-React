0e1a9eab0a36dd5dd354447b4c0ab3c1930a1b75
Johnatan Gonzalez Velez <jgonzalez@sistecredito.com>
2021-06-19 09:49:39 -0500

    Estructura de la calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,44 @@ import React from 'react'
 
 
 // generacion de la funcion componentes
+// Funcion fecha oArrow Fuction
+
 const App = () => {
+    console.log("Renderizacion de App")
     // lo que ejecuta la funcion
-    return <h1>Calculadora (App) </h1>
+    return (
+    <main className ='react-calculator' >
+        <div className= "result">
+        </div>
+        <div className= "numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+        </div>
+        <div className="functions">
+            <button>
+                Clear
+            </button>
+            <button>
+                R
+            </button>
+        </div>
+        <div className="math-operations">
+            <button>+</button>
+            <button>-</button>
+            <button>/</button>
+            <button>*</button>
+            <button>=</button>
+        </div>
+    </main>    
+    )
 }
 //  exportacion hacia otros componenets
 export default App
